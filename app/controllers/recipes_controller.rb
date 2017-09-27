abd82d077ebb1d0fbe2602d50e22edc9f724eaa1
ian-ramos <ramos.ianc@gmail.com>
2017-09-27 16:56:13 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.new # we need to intiialize 2 ingredients (per the spec), and the form will iterate through and create a line for each ingredient
+    @recipe.ingredients.new
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
+  end
+
 end
